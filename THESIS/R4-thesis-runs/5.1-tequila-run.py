d5abe2fe0e9ce1c8e3187f43fec47570d5bd0728
naglissul <naglis1134@gmail.com>
2025-05-09 17:13:54 +0200

    Add more thesis runs

---
+++ THESIS/R4-thesis-runs/5.1-tequila-run.py
@@ -0,0 +1,12 @@
+from tequila.quantumchemistry import Molecule
+
+geomstring="Li 0.0 0.0 0.0\nH 0.0 0.0 1.6"
+mol = Molecule(geometry=geomstring)
+
+H = mol.make_hamiltonian()
+U = mol.make_ansatz(name="SPA") # or e.g. UpCCGSD
+
+print("Hamiltonian:")
+print(H)
+print("Ansatz:")
+print(U)
